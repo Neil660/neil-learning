361b0e4593abf40672464f5ec01873a30d03c86b
Neil660 <huang.chengyi2@iwhalecloud.com>
2022-09-11 15:56:44 +0800

    Revert "Revert "eureaka""
    This reverts commit c3561bce

---
+++ neil-consumer/src/main/java/com/neil/consumer/NeilConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.neil.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeilConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeilConsumerApplication.class, args);
+    }
+
+}
---
+++ neil-consumer/src/test/java/com/neil/consumer/NeilConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neil.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeilConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
