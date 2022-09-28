a84be98f9c24acb6e4c3f83b23d56073a2d918dc
Neil660 <huang.chengyi2@iwhalecloud.com>
2022-09-28 15:14:37 +0800

    init

---
+++ neil-common/src/main/java/com/neil/common/NeilCommonApplication.java
@@ -0,0 +1,11 @@
+package com.neil.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeilCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NeilCommonApplication.class, args);
+    }
+}
