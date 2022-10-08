bb6fb66a50d7bc0cefa477c2318b7b29b584f525
Neil660 <huang.chengyi2@iwhalecloud.com>
2022-10-08 15:57:02 +0800

    整合mybatis

--- neil-common/src/main/java/com/neil/common/NeilCommonApplication.java
+++ neil-common/src/main/java/com/neil/common/NeilCommonApplication.java
@@ -1,8 +1,10 @@
 package com.neil.common;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan(value = "com.neil.common.dao")
 @SpringBootApplication
 public class NeilCommonApplication {
     public static void main(String[] args) {
--- neil-common/src/main/java/com/neil/common/controller/TestController.java
+++ neil-common/src/main/java/com/neil/common/controller/TestController.java
@@ -1,7 +1,15 @@
 package com.neil.common.controller;
 
+import com.neil.common.model.TestTable;
+import com.neil.common.service.TestService;
+import org.apache.ibatis.io.ResolverUtil.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @Decription
@@ -10,9 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  */
 @RestController
+@RequestMapping(value = "/test")
 public class TestController {
-    @RequestMapping
-    public String get() {
-        return "test";
+
+    @Autowired
+    TestService service;
+
+    @RequestMapping(value = "/getAllInfos")
+    public List<TestTable> getAllInfos() {
+        return service.getTestTable();
+    }
+
+    @RequestMapping(value = "/getById/{id}")
+    public String getById(@PathVariable int id) {
+        return service.getTestTableById(id).toString();
+    }
+
+/*    @RequestMapping(value = "/getById")
+    public String getById2(@RequestParam("id") int id) {
+        return service.getTestTableById(id).toString();
+    }*/
+
+    @RequestMapping(value = "/addTestTable")
+    public int addTestTable(@RequestParam("testTable") TestTable testTable) {
+        return service.addTestTable(testTable);
     }
 }
