f805a957d8d6631b0c458c20a0be5d8717233360
Neil660 <huang.chengyi2@iwhalecloud.com>
2022-11-21 10:15:04 +0800

    [spring-boot-mybatis]整合swagger2

--- spring-boot-mybatis/src/main/java/com/spring/boot/mybatis/controller/TestController.java
+++ spring-boot-mybatis/src/main/java/com/spring/boot/mybatis/controller/TestController.java
@@ -6,7 +6,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,31 +31,31 @@ public class TestController {
     @Autowired
     TestService service;
 
-    @RequestMapping(value = "/addTestTable")
+    @PostMapping(value = "/addTestTable")
     public int addTestTable(@RequestParam("testTable") TestTable testTable) {
         return service.addTestTable(testTable);
     }
 
-    @RequestMapping(value = "/addTestTable2")
+    @PostMapping(value = "/addTestTable2")
     public int addTestTable2() {
         return service.addTestTable2();
     }
 
     @ApiOperation(value = "获取用户信息", notes = "通过ID获取用户信息")
     @ApiImplicitParam(name = "id", value = "测试表ID",required = true, dataType = "Integer", paramType = "path")
-    @RequestMapping(value = "/getById/{id}")
+    @GetMapping(value = "/getById/{id}")
     public String getById(@PathVariable int id) {
         return service.getTestTableById(id).toString();
     }
 
-    @RequestMapping(value = "/getByName/{name}")
+    @GetMapping(value = "/getByName/{name}")
     public List<TestTable> getByName(@PathVariable String name) {
         return service.getTestTableByName(name);
     }
 
     // swagger注解
     @ApiOperation(value = "获取所有用户", notes = "获取所有用户")
-    @RequestMapping(value = "/getAllInfos")
+    @GetMapping(value = "/getAllInfos")
     public List<TestTable> getAllInfos() {
         return service.getTestTable();
     }
@@ -65,7 +67,7 @@ public class TestController {
         return service.getTestTableByOracle();
     }
 
-    @RequestMapping(value = "/get")
+    @GetMapping(value = "/get")
     public List<TestTable> get() {
         return service.get();
     }
